74fab4240e1c4d8eb479e423663ab6389f041dd5
chrisyip <yipchoyui1@gmail.com>
2024-05-17 12:47:00 +0800

    added themed Calendar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,13 +3,14 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import WixCalendar from '@/components/Calendar/WixCalendar';
+import ThemedCalendarScreen from '@/components/Calendar/test/_themedCalendarScreen';
 
 export default function TabOneScreen() {
+
   return (
-    <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)">
-    {/* <View style={styles.container}> */}
-      <WixCalendar />
-      {/* </View> */}
+    <View style={styles.separator} >
+      {/* <WixCalendar /> */}
+      <ThemedCalendarScreen />
     </View>
   );
 }
---
+++ components/Calendar/test/CalendarTheme.tsx
@@ -0,0 +1,40 @@
+/**
+ * Learn more about Light and Dark modes:
+ * https://docs.expo.io/guides/color-schemes/
+ */
+
+import { Text as DefaultText, View as DefaultView } from 'react-native';
+import { Calendar as DefaultCalendar, CalendarList as DefaultCalendarList  } from 'react-native-calendars';
+import { CalendarProps } from 'react-native-calendars/src/calendar/index';
+import { ContextProp } from 'react-native-calendars/src/types'
+
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '../../useColorScheme';
+import React, { useState, useEffect }from 'react';
+import { setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
+
+export interface themedCalendarProps extends CalendarProps {
+  lightColor?: string;
+  darkColor?: string;
+}
+
+export function useThemeColor(
+  props: { light?: string; dark?: string },
+  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+) {
+  const theme = useColorScheme() ?? 'light';
+  const colorFromProps = props[theme];
+
+  if (colorFromProps) {
+    return colorFromProps;
+  } else {
+    return Colors[theme][colorName];
+  }
+}
+
+export function Calendar(props: themedCalendarProps) {
+  const { style, lightColor, darkColor, ...otherProps } = props;
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+
+  return <DefaultCalendar key={color} theme={{calendarBackground: color}} {...otherProps} />
+}
