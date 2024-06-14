c4baf7e3a6e1e8f40fbb1780d903b95b0d793596
ChrisYip <yipchoyui1@gmail.com>
2024-06-14 13:05:03 +0800

    moved ctx and useStorageState to components

---
+++ components/Authentication/ctx.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useStorageState } from './useStorageState';
+
+const AuthContext = React.createContext<{
+  signIn: () => void;
+  signOut: () => void;
+  session?: string | null;
+  isLoading: boolean;
+}>({
+  signIn: () => null,
+  signOut: () => null,
+  session: null,
+  isLoading: false,
+});
+
+// This hook can be used to access the user info.
+export function useSession() {
+  const value = React.useContext(AuthContext);
+  if (process.env.NODE_ENV !== 'production') {
+    if (!value) {
+      throw new Error('useSession must be wrapped in a <SessionProvider />');
+    }
+  }
+
+  return value;
+}
+
+export function SessionProvider(props: React.PropsWithChildren) {
+  const [[isLoading, session], setSession] = useStorageState('session');
+  
+  return (
+
+    <AuthContext.Provider
+      value={{
+        signIn: () => {
+          // Perform sign-in logic here
+          console.log('signing in')
+          setSession('xxx');
+        },
+        signOut: () => {
+          console.log('signing Out')
+          setSession(null);
+        },
+        session,
+        isLoading,
+      }}>
+      {props.children}
+    </AuthContext.Provider>
+
+  );
+
+}
