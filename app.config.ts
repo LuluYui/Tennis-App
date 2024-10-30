bcb174300db9e846df5c31ad327b4aee6e789778
ChrisYip <yipchoyui1@gmail.com>
2024-10-31 00:07:01 +0800

    Changed name to Pussy League

--- app.config.ts
+++ app.config.ts
@@ -2,17 +2,17 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig=> ({
   ...config,
-  slug: 'my-nav-app',
-  name: 'my-nav-app',
+  slug: 'PussyLeague',
+  name: 'PussyLeague',
   android: {
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#ffffff"
       },
-      package: "com.luluyip.mynavapp",
+      package: "com.luluyip.pussyleague",
       googleServicesFile: process.env.GOOGLE_SERVICES_JSON
   },
   ios: {
-    bundleIdentifier: "com.luluyip.mynavapp"
+    bundleIdentifier: "com.luluyip.pussyleague"
   }
 });
