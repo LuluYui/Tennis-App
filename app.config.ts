3b7254af4690fcc8a338e0086520a1b39368e819
ChrisYip <yipchoyui1@gmail.com>
2024-10-31 10:07:40 +0800

    EAS BUILD - bcb1743 successful android 31/10/2024

--- app.config.ts
+++ app.config.ts
@@ -2,14 +2,14 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig=> ({
   ...config,
-  slug: 'PussyLeague',
+  slug: 'my-nav-app',
   name: 'PussyLeague',
   android: {
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#ffffff"
       },
-      package: "com.luluyip.pussyleague",
+      package: "com.luluyip.mynavapp",
       googleServicesFile: process.env.GOOGLE_SERVICES_JSON
   },
   ios: {
